b2391cc1a05dcfc4797ad41abfd9396d06ba3621
Martin Urban <github@gcal.ch>
2025-02-21 01:16:46 +0100

    test

--- src/index.js
+++ src/index.js
@@ -48,8 +48,23 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    connectTimeout: 10000,
+    debug: true
 }).promise();
+
+// Verbindung testen
+db.query('SELECT 1')
+  .then(() => {
+    console.log('Datenbankverbindung erfolgreich hergestellt');
+  })
+  .catch((err) => {
+    console.error('Fehler bei der Datenbankverbindung:', err);
+    process.exit(1);
+  });
 
 // API-Endpunkte mit Location-Parameter
 app.get('/api/drinks/:location', async (req, res) => {
